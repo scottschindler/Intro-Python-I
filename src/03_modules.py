82bc2daf1d8eaffaf381b771a7fae9a1b2c64bff
Scott Schindler <scottschindler29@gmail.com>
2020-01-15 21:22:59 -0500

    completed first 3 challenges

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,35 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print(str(sys.argv))
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print(sys.version)
 
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
+print(os.name)
 
 # Print the current process ID
 # YOUR CODE HERE
+print (os.getpid())
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
 
 # Print out your machine's login name
 # YOUR CODE HERE
+
+import getpass
+username = getpass.getuser()
+print(username)
+
+
+# come back to this one to correct for potentially incorrect ones. 
