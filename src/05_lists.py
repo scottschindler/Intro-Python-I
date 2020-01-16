9a59661c202845860518b6603ec41c2a40d6cb0c
Scott Schindler <scottschindler29@gmail.com>
2020-01-16 18:32:16 -0500

    completed challenges 1-7

--- src/05_lists.py
+++ src/05_lists.py
@@ -7,23 +7,30 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE
-print(x)
+print(x.append(4))
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+print(x + y)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+print(x + y[1:])
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.extend((4,8,88))
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+
+
+for value in x:
+  print(value * 100)
+
+#come back here to check answers
